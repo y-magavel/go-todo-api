ce0c901ac760a678a2b1878a4b369e650a12e954
y-magavel <magavel.dev@gmail.com>
2022-08-25 20:43:49 +0900

    外部から終了を指示されたときサーバーを終了する「run」関数を実装
    CHAPTER14 SECTION-055 pp.130-132

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"golang.org/x/sync/errgroup"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	)
-	if err != nil {
+	if err := run(context.Background()); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+
+	// 別ゴルーチンでHTTPサーバーを起動する
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルからの通知（終了通知）を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+		return err
+	}
+
+	// Goメソッドで起動した別ゴルーチンの終了を待つ
+	return eg.Wait()
 }
