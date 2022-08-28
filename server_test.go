9e51e86e099d8780fc3d428fb0425d8fc4dbc697
y-magavel <magavel.dev@gmail.com>
2022-08-28 18:22:52 +0900

    Server型を作りHTTPサーバーに関わる部分を分割
    CHAPTER16 SECTION-062 pp.159-161

--- server_test.go
+++ server_test.go
@@ -10,9 +10,7 @@ import (
 	"testing"
 )
 
-func TestRun(t *testing.T) {
-	t.Skip("リファクタリング中")
-
+func TestServer_Run(t *testing.T) {
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("failed to listen: %v", err)
@@ -20,8 +18,13 @@ func TestRun(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
 	eg.Go(func() error {
-		return run(ctx)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 
 	in := "message"
