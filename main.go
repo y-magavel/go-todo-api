c389cd2a64d194d064e8dc5c743ee48765af6abe
y-magavel <magavel.dev@gmail.com>
2022-08-28 18:49:12 +0900

    Server型とNewMux関数を利用してrun関数のリファクタリング
    CHAPTER16 SECTION-064 p.164

--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/y-magavel/go-todo-api/config"
-	"golang.org/x/sync/errgroup"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
 func main() {
@@ -22,44 +17,21 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
 	}
+
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
+
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with %v", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// コマンドラインで実験するため
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
+	mux := NewMux()
+	s := NewServer(l, mux)
 
-	// 別ゴルーチンでHTTPサーバーを起動する
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-		return err
-	}
-
-	// Goメソッドで起動した別ゴルーチンの終了を待つ
-	return eg.Wait()
+	return s.Run(ctx)
 }
