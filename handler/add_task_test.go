b9ef63a7f19b865d1aa3558e546553306f32ed8f
y-magavel <magavel.dev@gmail.com>
2022-09-05 01:42:29 +0900

    タスクを登録するエンドポイントに対するテストコードを作成
    CHAPTER17 SECTION-068 pp.174-175

---
+++ handler/add_task_test.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-playground/validator"
+	"github.com/y-magavel/go-todo-api/entity"
+	"github.com/y-magavel/go-todo-api/store"
+	"github.com/y-magavel/go-todo-api/testutil"
+)
+
+func TestAddTask(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		status  int
+		rspFile string
+	}
+	tests := map[string]struct {
+		reqFile string
+		want    want
+	}{
+		"ok": {
+			reqFile: "testdata/add_task/ok_req.json.golden",
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/add_task/ok_rsp.json.golden.golden",
+			},
+		},
+		"bad request": {
+			reqFile: "testdata/add_task/bad_req.json.golden",
+			want: want{
+				status:  http.StatusBadRequest,
+				rspFile: "testdata/add_task/bad_rsp.json.golden",
+			},
+		},
+	}
+
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodPost, "/tasks", bytes.NewReader(testutil.LoadFile(t, tt.reqFile)))
+
+			sut := AddTask{
+				Store: &store.TaskStore{
+					Tasks: map[entity.TaskID]*entity.Task{},
+				},
+				Validator: validator.New(),
+			}
+
+			sut.ServeHTTP(w, r)
+
+			resp := w.Result()
+			testutil.AssertResponse(t, resp, tt.want.status, testutil.LoadFile(t, tt.want.rspFile))
+		})
+	}
+}
