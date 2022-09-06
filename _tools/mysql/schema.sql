bc3203ae4c34a7ce0b881b0c60052d2c2a7b444e
y-magavel <magavel.dev@gmail.com>
2022-09-06 23:37:40 +0900

    userテーブルとtaskテーブルのDDLファイルを作成
    CHAPTER18 SECTION-072 p.184

---
+++ _tools/mysql/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `user`
+(
+    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユーザーの識別子',
+    `name`     VARCHAR(20)     NOT NULL COMMENT 'ユーザー名',
+    `password` VARCHAR(80)     NOT NULL COMMENT 'パスワードハッシュ',
+    `role`     VARCHAR(80)     NOT NULL COMMENT 'ロール',
+    `created`  DATETIME(6)     NOT NULL COMMENT 'レコード作成日時',
+    `updated`  DATETIME(6)     NOT NULL COMMENT 'レコード修正日時',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uix_name` (`name`) USING BTREE
+) Engine = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='ユーザー';
+
+CREATE TABLE `todo`
+(
+    `id`      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'タスクの識別子',
+    `title`   VARCHAR(128)    NOT NULL COMMENT 'タスクのタイトル',
+    `status`  VARCHAR(20)     NOT NULL COMMENT 'タスクの状態',
+    `created` DATETIME(6)     NOT NULL COMMENT 'レコード作成日時',
+    `updated` DATETIME(6)     NOT NULL COMMENT 'レコード修正日時',
+    PRIMARY KEY (`id`)
+) Engine = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='タスク';
