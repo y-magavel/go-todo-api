59c665a3ae275110664ef245bb46846ff72f95c4
y-magavel <magavel.dev@gmail.com>
2022-09-09 21:35:40 +0900

    インターフェースとRepository型、clockパッケージを定義
    CHAPTER18 SECTION-073 pp.192-194

--- store/repository.go
+++ store/repository.go
@@ -7,8 +7,41 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/y-magavel/go-todo-api/clock"
 	"github.com/y-magavel/go-todo-api/config"
 )
+
+type Beginner interface {
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
+
+type Preparer interface {
+	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	QueryxContext(ctx context.Context, query string, args ...any) (*sqlx.Rows, error)
+	QueryRowxContext(ctx context.Context, query string, args ...any) *sqlx.Row
+	GetContext(ctx context.Context, dest interface{}, query string, args ...any) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
+}
+
+var (
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.Tx)(nil)
+)
+
+type Repository struct {
+	Clocker clock.Clocker
+}
 
 func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	// sqlx.Connectを使うと内部でpingする。
