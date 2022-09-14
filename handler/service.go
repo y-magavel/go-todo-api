d5a2d4f714d7258a70d293fc57a8ab8698890d67
y-magavel <magavel.dev@gmail.com>
2022-09-14 22:59:42 +0900

    go generateコマンドを記述しているコメント行を修正
    コメント行の先頭にスペースがあるとモックコードが自動生成されないため

--- handler/service.go
+++ handler/service.go
@@ -6,7 +6,7 @@ import (
 	"github.com/y-magavel/go-todo-api/entity"
 )
 
-// go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
+//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
 type ListTasksService interface {
 	ListTasks(ctx context.Context) (entity.Tasks, error)
 }
