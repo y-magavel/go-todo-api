b316e48f76fe4595535634e4d7f485f93d10c3a4
y-magavel <magavel.dev@gmail.com>
2022-09-20 03:13:17 +0900

    ログイン用（POST /login）のエンドポイントを追加
    CHAPTER20 SECTION-085 p.250

--- mux.go
+++ mux.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator"
+	"github.com/y-magavel/go-todo-api/auth"
 	"github.com/y-magavel/go-todo-api/clock"
 	"github.com/y-magavel/go-todo-api/config"
 	"github.com/y-magavel/go-todo-api/handler"
@@ -26,7 +27,19 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	if err != nil {
 		return nil, cleanup, err
 	}
-	r := store.Repository{Clocker: clock.RealClocker{}}
+	clocker := clock.RealClocker{}
+	r := store.Repository{Clocker: clocker}
+	rcli, err := store.NewKVS(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	jwter, err := auth.NewJWTer(rcli, clocker)
+	if err != nil {
+		return nil, cleanup, err
+	}
+
+	l := &handler.Login{Service: &service.Login{DB: db, Repo: &r, TokenGenerator: jwter}, Validator: v}
+	mux.Post("/login", l.ServeHTTP)
 
 	at := &handler.AddTask{Service: &service.AddTask{DB: db, Repo: &r}, Validator: v}
 	mux.Post("/tasks", at.ServeHTTP)
